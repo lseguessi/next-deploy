13b770195f9242b1b76c2b78c1c64766664c2784
Lucas Seguessi <lucasseguessi@Lucass-MacBook-Pro.local>
2023-11-16 09:25:46 -0300

    change next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    compiler: {
+        styledComponents: true,
+      },
+    images: {
+        unoptimized: true,
+      },
+      trailingSlash: true,
+}
 
 module.exports = nextConfig
