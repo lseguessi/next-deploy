4706f574c189c3893b28ea4ed4a2dcf135ea0188
Lucas Seguessi <lucasseguessi@Lucass-MacBook-Pro.local>
2023-11-16 09:19:20 -0300

    init

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wctP/QDDwV93BLfYGohMmLeFn8ctjVI/9LFgEORCRqw=\"\n}"
---
+++ src/app/teste/page.tsx
@@ -0,0 +1,12 @@
+import Accordion from "@/components/Accordion";
+import NavBar from "@/components/NavBar";
+import React from "react";
+
+export default function Teste() {
+  return (
+    <div>
+      <NavBar />
+      <Accordion />
+    </div>
+  );
+}
